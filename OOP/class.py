30240c29fcbad395c9d438551854d697c98a5dfc
Ruheena-Tanveer <20am1a3127@svrec.ac.in>
2023-09-26 21:47:18 +0530

    first

---
+++ OOP/class.py
@@ -0,0 +1,18 @@
+class student_goal:
+    name="Sai prasanna"
+    occua="actor"
+    salary=20000
+    def info(self):
+        print(f"hello{a.name}")
+a=student_goal()
+a.name="a"
+print(a.name)
+#a.name="ritesh"
+#print(a.name,a.occua)
+#a.info()
+#def info(self):
+#print(f"{self.name} is an {self.occua}")
+b=student_goal()
+b.name="riut"
+print(b.name)
+b.info()
